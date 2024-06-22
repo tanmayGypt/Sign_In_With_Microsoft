0c9c9c4d564358c4ea1e4178f63d69401f1c0451
tanmayGypt <tanmay631@gmail.com>
2024-06-22 19:24:01 +0530

    button added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="">
+      <button className="p-4 bg-red-300">Sign In</button>
+    </div>
+  );
+};
+
+export default App;
